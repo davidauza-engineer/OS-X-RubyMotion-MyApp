e0389c08aa0241d96bc12b6a84bf775723cdc9e1
davidauza.engineer <davidauza01@gmail.com>
2021-01-19 09:13:01 -0500

    Update app/views/main_view.rb

--- app/views/main_view.rb
+++ app/views/main_view.rb
@@ -6,7 +6,7 @@ class MainView < NSView
   end
 
   def loadFriendsLabel
-    label = NSTextField.alloc.initWithFrame([[20, 30], [100, 100]])
+    label = NSTextField.alloc.initWithFrame([[20, 300], [100, 100]])
     label.stringValue = 'Descargar lista de amigos'
     label.sizeToFit
     label.editable = false
