f7ccddf2f731a4c2fa9239d45b15cda7bbfa4b66
davidauza.engineer <davidauza01@gmail.com>
2021-01-19 09:13:28 -0500

    Update main view controller

--- app/controllers/main_view_controller.rb
+++ app/controllers/main_view_controller.rb
@@ -2,5 +2,19 @@ class MainViewController < NSViewController
 
   def loadView
     self.view = MainView.alloc.initWithFrame(CGRect.new([0, 0], [200, 360]))
+    loadFriendsButton
+  end
+
+  def loadFriendsButton
+    button = NSButton.alloc.initWithFrame([[20, 250], [70, 30]])
+    button.target = self
+    button.action = :request_friends
+    button.title = 'Amigos'
+    
+    view.addSubview(button)
+  end
+
+  def request_friends
+    puts Friend.request
   end
 end
