ce9048459134cc3808548b1a9dcb87ed1c8749da
davidauza.engineer <davidauza01@gmail.com>
2021-01-19 08:37:39 -0500

    Add main view

---
+++ app/views/main_view.rb
@@ -0,0 +1,18 @@
+class MainView < NSView
+
+  def initWithFrame(frameRect)
+    super
+    loadFriendsLabel
+  end
+
+  def loadFriendsLabel
+    label = NSTextField.alloc.initWithFrame([[20, 30], [100, 100]])
+    label.stringValue = 'Descargar lista de amigos'
+    label.sizeToFit
+    label.editable = false
+    label.bordered = false
+    label.backgroundColor = NSColor.clearColor
+
+    addSubview(label)
+  end
+end
